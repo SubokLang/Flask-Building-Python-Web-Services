0b8be29cd3a5fa3cc1669e8ca1ae6e646f149244
SubokLang <lamdor2010@gmail.com>
2017-09-09 23:33:08 +0000

    Initial commit

---
+++ headlines.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template
+import feedparser
+
+app = Flask(__name__)
+
+RSS_FEEDS = {'bbc':"http://feeds.bbci.co.uk/news/rss.xml",
+             'cnn':'http://rss.cnn.com/rss/edition.rss',
+             'fox':'http://feeds.foxnews.com/foxnews/latest',
+             'iol':'http://www.iol.co.za/cmlink/1.640'}
+
+@app.route('/')
+@app.route('/<publication>') # <> defines variable
+def get_news(publication='bbc'):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    first_article = feed['entries'][10]
+    return render_template("home.html", article = first_article)
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
